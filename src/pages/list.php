6e8d45771d3181b02163d58589fa06e696fcbafa
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-02-07 18:23:34 +0100

    Update list.php

--- src/pages/list.php
+++ src/pages/list.php
@@ -5,7 +5,7 @@
         global $connection;
 
         //SQL-Zugriff auf Datensaetze
-        $query = $connection->prepare("SELECT id, code, creationdate FROM socialcodesm ORDER BY creationdate DESC");
+        $query = $connection->prepare("SELECT id, code, creationdate FROM socialcodes ORDER BY creationdate DESC");
         $query->execute();
         $query->setFetchMode(PDO::FETCH_ASSOC);
 
