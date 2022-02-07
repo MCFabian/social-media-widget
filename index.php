6a3dc3b715a1c2fe2f02ec7c3c1a60b12966bd08
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-02-07 17:32:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html id="themehere" class="dark">
 <head>
 	<meta charset="utf-8">
-	<title>Social Media Generator</title>
+	<title>Social Media Generator - PHP Version</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="src/css/style.css">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
