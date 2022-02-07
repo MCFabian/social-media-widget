e6d801350fd9fb8fb38dbdcb47f6a0fce1b76006
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-02-07 17:43:24 +0100

    style

--- src/pages/list.php
+++ src/pages/list.php
@@ -3,11 +3,13 @@
 ?>
 
 <div class="listcontainer">
-    <div>
-        <h2>Gespeicherten Codes</h2>
-        <p>In dieser Liste finden Sie alle gespeicherten Code-Kombinationen.</p>
+    <div id="listcontainerhead">
+        <div>
+            <h2>Gespeicherten Codes</h2>
+            <p>In dieser Liste finden Sie alle gespeicherten Code-Kombinationen.</p>
+        </div>
+        <div id="closelistcontainer"><i class="fas fa-times"></i></div>
     </div>
-    <div id="closelistcontainer"><i class="fas fa-times"></i></div>
     <hr>
     <ul>
         <li>Test</li>
