a216e5665dff951caa27afa07729d9fac644e6c6
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-02-07 18:18:49 +0100

    Update list.php

--- src/pages/list.php
+++ src/pages/list.php
@@ -1,28 +1,20 @@
 <?php include("connection.php") ?>
 
 <?php
-    function listfiles(){
+    function listcodes(){
         global $connection;
 
         //SQL-Zugriff auf Datensaetze
-        $query = $connection->prepare("SELECT fileid AS fileid, nameoffile AS fileentry, editor AS editorid, vname, nname FROM T_Files, T_Persons WHERE editor = p_person_nr ORDER BY fileid DESC");
+        $query = $connection->prepare("SELECT id, code, creationdate FROM socialcodes ORDER BY DESC");
         $query->execute();
         $query->setFetchMode(PDO::FETCH_ASSOC);
 
 
         while ($row = $query->fetch()){
-            $id						= $row['fileid'];
-            $fileentry				= $row['fileentry'];
-            $fileentry				= $row['fileentry'];
-            if(empty($fileentry)){
-                $gotofile = "<i>Keine Bezeichnung</i>";
-            }
-
-            if(!empty($fileentry)){
-                $gotofile = "<a target='_blank' href='/pointer/documents/uploaded/$fileentry'>$fileentry</a>";
-            }
-            $creatorid              = $row['editorid'];
-            $creatorname            = $row['vname']." ".$row['nname'];
+            $id						    = $row['id'];
+            $code				        = $row['code'];
+            $creationdate				= $row['creationdate'];
+           
 
             if($id < 0){
                 echo "leer";
@@ -30,10 +22,7 @@
 
             else{
 
-            echo"
-
-            <li>$gotofile -  <a href='/pointer/documents/uploaded/$fileentry' download class='icon right setting-interaction'>file_download</a></li>
-            <";
+            echo"<li><strong>$id</strong> - Erstellt am: $creationdate</li>";
             }
 
 
@@ -53,6 +42,6 @@
     <ul>
         <li>Test</li>
         <li>Test</li>
-        <?php listfiles(); ?>
+        <?php listcodes(); ?>
     </ul>
 </div>
