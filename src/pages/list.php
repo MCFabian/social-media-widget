56b984d572d11c9c99ca5a4cc0719faf4e00cb9c
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-02-07 18:10:17 +0100

    a

--- src/pages/list.php
+++ src/pages/list.php
@@ -49,16 +49,15 @@
     }
 
 ?>
-
-<div class="listcontainer">
+<div id="codelib" class="listcontainer">
     <div id="listcontainerhead">
         <div>
             <h2>Gespeicherten Codes</h2>
-            <p>In dieser Liste finden Sie alle gespeicherten Code-Kombinationen. Um einen Code zu laden, klicken Sie diesen an.</p>
+            <p>In dieser Liste finden Sie alle gespeicherten Code-Kombinationen.</p>
         </div>
-        <div id="closelistcontainer"><i class="fas fa-times"></i></div>
+        <div onclick="opencodelib();" id="closelistcontainer"><i class="fas fa-times"></i></div>
     </div>
-    <hr>
+
     <ul>
         <li>Test</li>
         <li>Test</li>
