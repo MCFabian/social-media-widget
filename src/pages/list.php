e380096eb6f305942bbb51986a186ac901be292d
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-02-07 17:40:37 +0100

    Close

--- src/pages/list.php
+++ src/pages/list.php
@@ -3,6 +3,12 @@
 ?>
 
 <div class="listcontainer">
+    <div>
+        <h2>Gespeicherten Codes</h2>
+        <p>In dieser Liste finden Sie alle gespeicherten Code-Kombinationen.</p>
+    </div>
+    <div id="closelistcontainer"><i class="fas fa-times"></i></div>
+    <hr>
     <ul>
         <li>Test</li>
         <li>Test</li>
