253604348409ce1d543b6ca1cc8e5e6375595dfa
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-02-15 17:16:48 +0100

    s

--- index.php
+++ index.php
@@ -836,12 +836,12 @@
 	</div>
 	<div class="fab-group">
 		<a id="savecode" class="fab"><i class="fas fa-save"></i>Code speichern</a>
+		<a title="Dark/Light Mode" class="fab" onclick="setTheme()"><i class="fas fa-adjust"></i></a>
 		<div id="morebtn" class="fab"><i class="fas fa-ellipsis-v"></i>Mehr</div>
 		<ul id="moremenu">
 				<li><a onclick="popup()" id="" class="" title="What's new?"><i class="fas fa-certificate" ></i>Updates</a></li>
 				<li><a target="_blank" href="https://github.com/MCFabian/social-media-widget/issues/new" title="Bug melden via GitHu"><i class="fas fa-bug"></i>Fehler melden</a></li>
 		</ul>
-		<a title="Dark/Light Mode" class="fab" onclick="setTheme()"><i class="fas fa-adjust"></i></a>
 	</div>
 
 	<script src="src/js/script.js"></script>
