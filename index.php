b202169981279802480de1544a2eef4d4e537de7
Fabian <60275181+MCFabian@users.noreply.github.com>
2022-02-23 15:08:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -792,7 +792,7 @@
 				<h2>Vorschau</h2>
 				<div id="socialmedialistdata">
 					<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
-					<div id="socialmedialist" class="basic">
+					<div id='socialmedialist' class="basic">
 
 					</div>
 				</div>
