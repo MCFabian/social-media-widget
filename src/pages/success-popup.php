42227550c1edae61fc497234d0c2d663bac2abb3
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-03-01 16:00:13 +0100

    test

--- src/pages/success-popup.php
+++ src/pages/success-popup.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="../css/style.css">
 
 <?php
     include("pagehead.php");
@@ -7,14 +7,13 @@
 
 
 <body>
-    <div id="popupcontainer" class="popupcontainer">
+
         <div class="popup">
             <h2>Code erfolgreich gepseichert!</h2>
-            <div id="success-icon"><i class="fa-solid fa-check"></i></div>
-            <?php echo"<p>Der Code wurde erfolgreich gespeichert! Deine gespeicherte ID lautet: <strong>$myc</strong></p> ";?>
+            <?php echo"<p class='hint'><i class='fa-check'> Der Code wurde erfolgreich gespeichert! Deine gespeicherte ID lautet: <strong>$myc</strong></p> ";?>
             <hr>
-            <a class="buttons" href="/">Startseite</a>
+            <a class="button" href="/">Startseite</a>
         </div>
-    </div>
+
 </body>
 
