b625978ad7886855282350e6cfd8d4b4ba15e64f
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-03-02 14:18:40 +0100

    style

--- src/pages/success-popup.php
+++ src/pages/success-popup.php
@@ -7,14 +7,14 @@
 
 
 <body>
-    <div class="row center-box full-height">
+    <div class="row full-height flex">
         <div class="popup center-box">
             <div class="big-icon">
                 <i class='fas fa-check big-icon'></i>
             </div>
             <h2 class="center">Code erfolgreich gepseichert!</h2>
             <hr>
-            <?php echo"<p class='larger-text'>Der Code wurde erfolgreich gespeichert! Deine gespeicherte ID lautet: <strong>$myc</strong></p> ";?>
+            <?php echo"<p class='larger-text'>Der Code wurde erfolgreich gespeichert! Deine gespeicherte ID lautet:<br> <strong class='chip'>$myc</strong></p> ";?>
             <div class="btn-group center">
                 <a class="button" href="/">Neuen Code erstellen</a>
             </div>
