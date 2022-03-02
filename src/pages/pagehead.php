4c5dd39cd00725c2761f7a9b83ca91b069559569
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-03-02 14:51:17 +0100

    Update pagehead.php

--- src/pages/pagehead.php
+++ src/pages/pagehead.php
@@ -11,5 +11,5 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Inria+Sans:wght@300;400;700&display=swap" rel="stylesheet">
 	<link type="image/x-icon"rel="shortcut icon" href="<?php echo"$_SERVER['DOCUMENT_ROOT'].'/src/img/gen.png'";?>">
-
+	<?php echo"$_SERVER[DOCUMENT_ROOT]"; ?>
 </head>
