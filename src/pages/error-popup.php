a2e57462fa786559142a154b649fbe69621e4a35
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-03-02 14:55:28 +0100

    s

--- src/pages/error-popup.php
+++ src/pages/error-popup.php
@@ -9,7 +9,7 @@
 <body>
     <div class="full-height flex">
         <div class="popup center-box">
-            <div class="big-icon error">
+            <div id="error" class="big-icon">
                 <i class="fas fa-face-anguished"></i>
             </div>
             <h2 class="center">Es gab Fehler :/</h2>
