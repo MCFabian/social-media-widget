6eb63bf398e486121132d65a019d590e5682c56f
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-03-02 14:54:41 +0100

    Update pagehead.php

--- src/pages/pagehead.php
+++ src/pages/pagehead.php
@@ -1,15 +1,15 @@
 <!doctype html>
 <html id="themehere" class="dark">
 <head>
+	<base href="https://www.dev-social-media.fabiar.de/"/> 	
 	<meta charset="utf-8">
 	<meta lang="de">
 	<title>Social Media Generator - PHP Version</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="<?php echo"$_SERVER[DOCUMENT_ROOT].'/src/css/style.css'";?>">
+	<link rel="stylesheet" href="src/css/style.css">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Inria+Sans:wght@300;400;700&display=swap" rel="stylesheet">
-	<link type="image/x-icon"rel="shortcut icon" href="<?php echo"$_SERVER[DOCUMENT_ROOT].'/src/img/gen.png'";?>">
-	<?php echo"$_SERVER[DOCUMENT_ROOT]"; ?>
+	<link rel="shortcut icon" href="src/img/gen.png" type="image/x-icon">
 </head>
