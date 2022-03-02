9c07eac8f3cbaae222f68840bc8d0575198cc7b5
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-03-02 14:00:50 +0100

    Update success-popup.php

--- src/pages/success-popup.php
+++ src/pages/success-popup.php
@@ -7,13 +7,13 @@
 
 
 <body>
-
+    <div class="row center-box">
         <div class="popup">
             <h2>Code erfolgreich gepseichert!</h2>
-            <?php echo"<p class='hint'><i class='fa-check'></i> Der Code wurde erfolgreich gespeichert! Deine gespeicherte ID lautet: <strong>$myc</strong></p> ";?>
+            <?php echo"<p class='hint'><i class='fas fa-check'></i> Der Code wurde erfolgreich gespeichert! Deine gespeicherte ID lautet: <strong>$myc</strong></p> ";?>
             <hr>
-            <a class="button" href="/">Startseite</a>
+            <a class="button" href="/">Neuen Code erstellen</a>
         </div>
-
+    </div>
 </body>
 
