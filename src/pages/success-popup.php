a672fb0cd90072f7ab08d0f389decbb92e1abb5a
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-03-04 14:11:46 +0100

    copy code

--- src/pages/success-popup.php
+++ src/pages/success-popup.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="../css/style.css">
-
+<script src="../js/script.js"></script>
 <?php
     include("pagehead.php");
 ?>
@@ -14,11 +14,27 @@
             </div>
             <h2 class="center">Code gespeichert!</h2>
             <hr>
-            <?php echo"<p class='larger-text'>Der Code wurde erfolgreich gespeichert! Deine gespeicherte ID lautet:<br> <strong class='chip'>$myc</strong></p> ";?>
+            <?php echo"<p class='larger-text'>Der Code wurde erfolgreich gespeichert! Deine gespeicherte ID lautet:<br> <strong id='creationcode' class='chip'>$myc</strong></p> ";?>
             <div class="btn-group center">
                 <a class="button" href="/"><i class="fas fa-plus"></i>Neuen Code erstellen</a>
             </div>
         </div>
     </div>
 </body>
+
+<script>
+
+    document.getElementById("creationcode").addEventListener("click", function(){
+        var code = document.getElementById("creationcode").innerHTML;
+        
+        code.select();
+        code.setSelectionRange(0, 99999); /* For mobile devices */
+
+        /* Copy the text inside the text field */
+        navigator.clipboard.writeText(code);
+
+        notification('success', 'Der Code wurde erfolgreich in die Zwischenablage kopiert!');
+    })
+
+</script>
 
