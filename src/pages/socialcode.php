d4a1bab58fae0a83ef0b7d37a33ec797e6982db3
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-03-10 14:49:23 +0100

    a

--- src/pages/socialcode.php
+++ src/pages/socialcode.php
@@ -14,9 +14,9 @@
 <?php
     $socialdata = getMediaData($_GET["id"]);
     //if(!isSet($socialdata)) MediatNotFound();
-
-
 ?>
 
 <script src="../js/script.js"></script>
 <script src="../js/gen.js"></script>
+
+<script>document.getElementById('generate').click();</script>
