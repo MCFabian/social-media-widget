0a163665377b194c7b8b834556d20fca4420056a
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2022-03-21 13:52:36 +0100

    Update pagehead.php

--- src/pages/pagehead.php
+++ src/pages/pagehead.php
@@ -31,3 +31,37 @@
 			}
 		);
 	</script>
+	
+	<script>
+		function setCookie(cvalue) {
+  var d = new Date();
+  d.setTime(d.getTime() + (30 * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = "theme=" + cvalue + ";" + expires + "; path=/";
+}
+function getCookie(theme) {
+  var name = theme + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function checkCookie() {
+  var theme = getCookie("theme");
+  if (theme == "light") {
+  body.className = "light";
+  }
+  if (theme == "dark") {
+  body.className = "dark";
+  }  
+}
+		
+	</script>
